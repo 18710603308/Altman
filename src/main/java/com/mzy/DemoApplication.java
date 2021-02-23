cc20050f09c6d5a00bec4bd4d096b08023396337
miaozhuoyang <123456>
2021-02-23 14:49:41 +0800

    test

---
+++ src/main/java/com/mzy/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.mzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Jack Miao
+ * @date 2021/1/11 20:50
+ * @desc
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class);
+    }
+}
